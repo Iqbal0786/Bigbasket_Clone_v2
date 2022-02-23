0e649b414265a2fd333b2e2a8748c0d1f2c0532b
Mohammad Iqbal <iqbal240456@gmail.com>
2022-02-24 00:37:20 +0530

    added login/signup page

---
+++ scripts/signup.js
@@ -0,0 +1,5 @@
+function registerUser(){
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+}
