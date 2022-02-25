bbca797358d53c02a8e463c254cea944c838202c
prajwal0403 <rautprajwal546@gmail.com>
2022-02-25 11:26:24 +0530

    da

--- src - Copy/configs/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose= require("mongoose");
-
-module.exports= ()=>{
-    return mongoose.connect("mongodb+srv://iqbal:iqbal_786@cluster0.daqxb.mongodb.net/BigbasketDB?retryWrites=true&w=majority")
-}
